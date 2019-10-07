e06cc2f7d8306f9738b0373dd341a8b3f42462bc
Cristian Castellanos <cristiancastellanos@wposs.com>
2019-10-07 07:45:33 -0500

    button.dart -> Se crea el archivo button.dart, donde crearemos la clase MyButton y la clase _MyButtonState, donde crearemos una columna, un text, un botón y modificaremos el color Scaffold y daremos un título.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stateful_app/main.dart';
+import 'package:stateful_app/src/button.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyButton());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
